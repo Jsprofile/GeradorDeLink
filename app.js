cad83282ef30ac9a59b4b28658c16faeaf8c41e6
Jsprofile <jsdeluca01@gmail.com>
2022-10-23 02:07:38 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const celId = document.getElementById('Cellphone')
+const MsgId = document.getElementById('Message')
+const btnGenerate = document.querySelector('button')
+const resultBox = document.getElementById('resultBox')
+const PosResult = document.querySelector('.PosResult')
+const btnBatman = document.querySelector('.btnBatman')
+
+btnGenerate.addEventListener('click', () => {
+    var msg = MsgId.value
+    var cell = celId.value.replace(/\s/g, '');
+    encodedMsg = encodeURI(msg)
+
+    resultBox.innerHTML = `https://api.whatsapp.com/send?phone=55${cell}&text=${encodedMsg}`
+
+
+    PosResult.style.animation = "shower .5s forwards"
+
+
+    var generateSpan = document.querySelector('button span')
+
+    navigator.clipboard.writeText(resultBox.innerHTML).then(() => {
+        generateSpan.innerText = 'Copiado!'
+    })
+
+
+   
+
+})
